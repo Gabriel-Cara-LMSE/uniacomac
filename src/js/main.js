20d9bab416530efa615dfa57bbc516e8c2a3df8a
Gabriel-Cara-LMSE <gabriel.cara.lmse@gmail.com>
2023-08-25 11:28:58 -0300

    finished responsive: waiting for investiment

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,16 @@
 /**
    * Preloader
    */
-let preloader = document.querySelector('#preloader');
+let preloader = document.querySelector("#preloader");
 if (preloader) {
-  window.addEventListener('load', () => {
+  window.addEventListener("load", () => {
     preloader.remove()
   });
 }
+
+// Responsive Navbar
+
+const hamburger = document.querySelector(".hamburger")
+const nav = document.querySelector(".nav")
+
+hamburger.addEventListener("click", () => nav.classList.toggle("active"))
